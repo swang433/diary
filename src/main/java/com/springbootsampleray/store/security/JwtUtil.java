4da95524d0f5cb27c911ddc747f6f94a0c9a4696
swang433 <rw091402@gmail.com>
2026-06-09 18:16:07 +0800

    refactor: remove unused imports and clean up SecurityConfig and JwtUtil for improved code clarity

--- src/main/java/com/springbootsampleray/store/security/JwtUtil.java
+++ src/main/java/com/springbootsampleray/store/security/JwtUtil.java
@@ -4,7 +4,6 @@ import java.util.Date;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.JWTVerifier;
 import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Value;
--- src/main/java/com/springbootsampleray/store/security/SecurityConfig.java
+++ src/main/java/com/springbootsampleray/store/security/SecurityConfig.java
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;;
 
@@ -19,17 +17,9 @@ also allows JwtUtil.java to inject the password encoder bean
 @EnableWebSecurity
 public class SecurityConfig {
     private final JwtFilter jwtFilter;
-    private final JwtUtil jwtUtil;
 
-    SecurityConfig(JwtUtil jwtUtil, JwtFilter jwtFilter) {
-        this.jwtUtil = jwtUtil;
+    SecurityConfig(JwtFilter jwtFilter) {
         this.jwtFilter = jwtFilter;
-    }
-
-    @Bean
-    public PasswordEncoder Encoder()
-    {
-        return new BCryptPasswordEncoder(); 
     }
 
     @Bean
